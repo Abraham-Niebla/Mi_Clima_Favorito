ecde3c3bca7b07581cfdd55ec21e346f7508add6
Abraham Niebla <a1176033@uabc.edu.mx>
2025-06-01 20:46:12 -0700

    Valores globales

--- app/src/main/java/uabc/miclimafavorito/backend/apiService/fetchWeatherData.kt
+++ app/src/main/java/uabc/miclimafavorito/backend/apiService/fetchWeatherData.kt
@@ -2,14 +2,17 @@ package uabc.miclimafavorito.backend.apiService
 
 import uabc.miclimafavorito.data.weather.SearchResponse
 import uabc.miclimafavorito.data.weather.WeatherResponse
+import uabc.miclimafavorito.globals.API_DAYS_FORECAST
+import uabc.miclimafavorito.globals.API_KEY
+import uabc.miclimafavorito.globals.API_LANG
 
 suspend fun fetchWeatherData(idCiudad: Long): WeatherResponse {
     return try {
         RetrofitInstance.api.getCurrentWeather(
-            apiKey = "06b8c71ee2f1412f8c871700252005",
+            apiKey = API_KEY,
             location = "id:$idCiudad" ,
-            language = "es",
-            days = 4
+            language = API_LANG,
+            days = API_DAYS_FORECAST
         )
     } catch (e: Exception) {
         WeatherResponse()
@@ -19,9 +22,9 @@ suspend fun fetchWeatherData(idCiudad: Long): WeatherResponse {
 suspend fun fetchCities(ciudad: String): List<SearchResponse> {
     return try {
         RetrofitInstance.api.getSearchCities(
-            apiKey = "06b8c71ee2f1412f8c871700252005",
+            apiKey = API_KEY,
             location = ciudad,
-            language = "es",
+            language = API_LANG,
         )
     } catch (e: Exception) {
         listOf(SearchResponse())
--- app/src/main/java/uabc/miclimafavorito/globals/Globals.kt
+++ app/src/main/java/uabc/miclimafavorito/globals/Globals.kt
@@ -4,4 +4,7 @@ import androidx.compose.ui.unit.dp
 
 const val BASE_API_URL = "https://api.weatherapi.com/"
 const val BASE_ICON_URL = "//cdn.weatherapi.com/weather/128x128"
+const val API_KEY = "06b8c71ee2f1412f8c871700252005"
+const val API_LANG = "es"
+const val API_DAYS_FORECAST = 4
 val ROUND_BORDERS_VALUE = 10.dp
