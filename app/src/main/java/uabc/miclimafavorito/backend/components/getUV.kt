0f0cb964b0fa54e471d2c4891566edef1fd11d50
Abraham Niebla <a1176033@uabc.edu.mx>
2025-06-02 02:40:17 -0700

    Solución con índice UV

--- app/src/main/java/uabc/miclimafavorito/backend/components/getUV.kt
+++ app/src/main/java/uabc/miclimafavorito/backend/components/getUV.kt
@@ -7,10 +7,10 @@ import uabc.miclimafavorito.R
 @Composable
 fun getUV(uv: Double): String{
     return when {
-        uv in 0.0..2.0      -> stringResource(R.string.uv_low)
-        uv in 3.0..5.0      -> stringResource(R.string.uv_moderate)
-        uv in 6.0..7.0      -> stringResource(R.string.uv_high)
-        uv in 8.0..10.0     -> stringResource(R.string.uv_very_high)
+        uv in 0.0..2.9      -> stringResource(R.string.uv_low)
+        uv in 3.0..5.9      -> stringResource(R.string.uv_moderate)
+        uv in 6.0..7.9      -> stringResource(R.string.uv_high)
+        uv in 8.0..10.9     -> stringResource(R.string.uv_very_high)
         uv >= 11.0          -> stringResource(R.string.uv_extreme)
         else                -> stringResource(R.string.uv_unknown)
     }
--- app/src/main/java/uabc/miclimafavorito/backend/components/getUVColor.kt
+++ app/src/main/java/uabc/miclimafavorito/backend/components/getUVColor.kt
@@ -2,17 +2,24 @@ package uabc.miclimafavorito.backend.components
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import uabc.miclimafavorito.R
 
 @Composable
 fun getUVColor(uv: Double): Color{
     return when {
-        uv in 0.0..2.0      -> Color(0xFF5ea500)
-        uv in 3.0..5.0      -> Color(0xFFd08700)
-        uv in 6.0..7.0      -> Color(0xFFf54900)
-        uv in 8.0..10.0     -> Color(0xFFec003f)
+        uv in 0.0..2.9      -> Color(0xFF5ea500)
+        uv in 3.0..5.9      -> Color(0xFFd08700)
+        uv in 6.0..7.9      -> Color(0xFFf54900)
+        uv in 8.0..10.9     -> Color(0xFFec003f)
         uv >= 11.0          -> Color(0xFF6e11b0)
         else                -> Color.Black
     }
 }
+/*
+        Índice UV	    Nivel de riesgo
+
+        0–2.9	            Bajo
+        3–5.9	            Moderado
+        6–7.9	            Alto
+        8–10.9	        Muy alto
+        11+	            Extremo
+*/
